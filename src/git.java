90e4f434a3a5492b33d1cdb6bbc38d3e35242108
yinjiahui <920388420@qq.com>
2019-02-21 23:11:48 +0800

    提交到仓库

---
+++ src/git.java
@@ -0,0 +1,10 @@
+public class git {
+    public static void main(String[] args) {
+        System.out.println("执行了");
+        System.out.println("执行了");
+        System.out.println("执行了");
+        System.out.println("执行了");
+        System.out.println("执行了");
+        System.out.println("执行了");
+    }
+}
